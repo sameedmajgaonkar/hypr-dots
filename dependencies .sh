70b88facec3f6e3c30d9e57ee9e0aaf30f22bf15
sameed1718 <sameed1718@gmail.com>
2024-05-05 23:42:53 +0530

    first commit

---
+++ dependencies .sh
@@ -0,0 +1,3 @@
+paru -S hyprland-git swww-git vscodium-bin zsh-autosuggestions rofi-power-menu rofi-wayland waybar gradience swaync whitesur-icon-theme vimix-cursors candy-icons-git brave
+sudo pacman -S python-pywal kitty brightnessctl grim slurp
+yay -S gradience-git
