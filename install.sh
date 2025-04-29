eb4ff6bffb5c8ec6d6889c8280fc4dcb8932159a
Sameed <120316421+sameedmajgaonkar@users.noreply.github.com>
2025-04-29 22:21:13 +0530

    Fix install script issues

--- install.sh
+++ install.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-if pacman -Qs paru >/dev/null; then
-  echo "paru is already installed"
-else
-  echo "paru not found. Installing paru..."
-  install_paru
-fi
-
-cd $HOME
-echo "Installing...."
-install_dependencies
-setup_terminal_theme
-setup_hyprland
-setup_theme
 
 install_paru() {
   sudo pacman -S --needed base-devel
@@ -21,13 +8,22 @@ install_paru() {
   makepkg -si
 }
 
+if pacman -Qs paru >/dev/null; then
+  echo "paru is already installed"
+else
+  echo "paru not found. Installing paru..."
+  install_paru
+fi
+
 install_dependencies() {
+  echo "Installing dependencies..."
   sudo pacman -Syy
-  paru -S hyprland-git swww-git vscodium-bin rofi-power-menu rofi-wayland waybar gradience swaync whitesur-icon-theme vimix-cursors kitty
+  paru -S hyprland-git swww-git vscodium-bin rofi-power-menu rofi-wayland waybar gradience swaync whitesur-icon-theme vimix-cursors kitty xdg-desktop-portal-hyprland rofi-bluetooth-git
   sudo pacman -S python-pywal kitty brightnessctl grim slurp pywalfox pipewire wireframe
 }
 
 setup_terminal_theme() {
+  echo "Configuring terminal..."
   sudo pacman -S zsh
   chsh -s $(which zsh)
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -36,12 +32,22 @@ setup_terminal_theme() {
 }
 
 setup_hyprland() {
+  echo "Copying config files..."
   git clone https://github.com/sameedmajgaonkar/hypr-dots.git
-  cp -ri $HOME/hypr-dots/* $HOME/
+  rsync -av --exclude=".git" --exclude="README.md" $HOME/hypr-dots/. $HOME/
 }
 
 setup_theme(){
+  echo "Setting up theme..."
   gsettings set org.gnome.desktop.interface cursor-theme 'Vimix Cursors - White' 
   gsettings set org.gnome.desktop.interface cursor-size 24
   sh $HOME/theme.sh
 }
+
+
+cd $HOME
+echo "Installation Begins!!!"
+install_dependencies
+setup_terminal_theme
+setup_hyprland
+setup_theme
